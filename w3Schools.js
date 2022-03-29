4172c3b328f996ea54408e6ade0b0ce0d75da96d
Destiney Rodney <destiney.rodney01@gmail.com>
2022-03-29 15:54:32 -0500

    feat: Javascript

---
+++ w3Schools.js
@@ -0,0 +1,25 @@
+// Create a variable called z, assign x + y to it, and display the result in an alert box.
+var x = 1;
+var y= 1;
+var z = x + y;
+alert(z);
+
+//On one single line, declare three variables with the following names and values:
+//
+// firstName = "John"
+// lastName = "Doe"
+// age = 35
+
+var firstName = "John", lastName = "Doe", age = 35;
+console.log(firstName, lastName, age);
+
+//Multiply 10 with 5, and alert the result:
+alert(10 * 5);
+
+//IIFE - immediately invoke function expression
+
+(function (){
+    let iffe = "Iffe local";
+    alert(iffe);
+})();
+
