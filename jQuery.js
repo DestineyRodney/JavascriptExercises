7e04f2c7e460e2b5bff11e8035e6eebc212f188b
Destiney Rodney <destiney.rodney01@gmail.com>
2022-04-03 22:50:07 -0500

    Selecting By id

---
+++ jQuery.js
@@ -0,0 +1,8 @@
+"use strict";
+
+$(document).ready(function(){
+    alert("Ready");
+}); // alerts page is loaded
+
+var myName = $('#name').html(); // html() method to get the contents of the HTML element as a string and then assigned that string to the variable myName
+console.log(myName);
