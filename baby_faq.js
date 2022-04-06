6ddaabccf816973360a40556b42adf338d376f94
Destiney Rodney <destiney.rodney01@gmail.com>
2022-04-05 20:42:03 -0500

    feat: Swapped images

--- baby_faq.js
+++ baby_faq.js
@@ -14,4 +14,45 @@ $('dt').click(function (e){
     $(this).toggleClass('highlight-dt');
 });
 
+$('#last-li').click(function(e){
+    e.preventDefault();
+    $('ul').each(function(){
+        $(this).children().last().addClass('highlight-last');
+    })
+
+})
+
+    $('h3').click(function(e){
+        console.log('clicked');
+        e.preventDefault();
+        $('li').css('font-weight', 'bold');
+    })
+
+    $('li').click(function(e){
+        e.preventDefault();
+        $(this).parent().children('li').first().css('color', 'blue');
+    })
+
+$('#btn1').click(function(e){
+    console.log('clicked');
+    e.preventDefault();
+    $('#img1').attr("src", "img/8.jpg");
+});
+
+    $('#btn-2').click(function(e){
+        console.log('clicked');
+        e.preventDefault();
+        $('#img2').attr("src", "img/7.jpg");
+    });
+
+    $('#btn-3').click(function(e){
+        console.log('clicked');
+
+        e.preventDefault();
+        $('#img3').attr("src", "img/Vocaroo%20QR%20Code%201h5UuqpfNIiD.png")
+    })
+
+
+
+
 });
